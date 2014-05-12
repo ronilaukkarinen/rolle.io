00bd080f256960edbe7dcabae411a4fba3bcbd81
Roni Laukkarinen <roni@laukkarinen.info>
2014-05-12 09:23:52 +0300

    etiäppäin

--- index.php
+++ index.php
@@ -1,185 +1,99 @@
-<?php
-require 'includes/config.php';
-require 'includes/aboutPage.class.php';
-require 'includes/vcard.class.php';
-
-$profile = new AboutPage($info);
-
-if(array_key_exists('json',$_GET)){
-	$profile->generateJSON();
-	exit;
-} 
-else if(array_key_exists('vcard',$_GET)){
-	$profile->downloadVcard();
-	exit;
-}
-?><!DOCTYPE html>
+<!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">          
+        <title>Front End Developer, Web designer &mdash; Roni Laukkarinen, Jyväskylä</title>
 
-        <title>Kotisivut, sosiaalinen media &mdash; Roni Laukkarinen, Jyväskylä</title>
+		<link rel="stylesheet" href="css/font-awesome.css" />
+        <link rel="stylesheet" href="fonts/proximanova/stylesheet.css" />
+        <link rel="stylesheet" href="fonts/minionpro/stylesheet.css" />
+        <link rel="stylesheet" href="fonts/gotham/stylesheet.css" />
+        <link rel="stylesheet" href="css/layout.css" />
 
-        <link rel="stylesheet" href="webfonts/proximanova/stylesheet.css" />
-        <link rel="stylesheet" href="webfonts/minionpro/stylesheet.css" />
-        <link rel="stylesheet" href="webfonts/gotham/stylesheet.css" />
-        <link rel="stylesheet" href="style.css" />
+        <link rel="shortcut icon" href="img/favicon.ico" />
+		<link rel="icon" href="img/favicon.png" type="image/png" /> 
+		<link rel="apple-touch-icon" href="img/iphone-touch-icon.png" />
+		<link rel="apple-touch-icon" sizes="114x114" href="img/iphone-touch-icon.png" />
 
-        <link rel="shortcut icon" href="favicon.ico" />
-		<link rel="icon" href="favicon.png" type="image/png" /> 
-		<link rel="apple-touch-icon" href="iphone-touch-icon.png" />
-		<link rel="apple-touch-icon" sizes="114x114" href="iphone-touch-icon.png" />
-
-		<script src="js/jquery-1.9.0.min.js"></script>
-
-		<link rel="stylesheet" href="/js/fancybox/jquery.fancybox.css?v=2.1.4" type="text/css" media="screen" />
-		<script src="/js/fancybox/jquery.fancybox.pack.js?v=2.1.4"></script>
-
-		<link rel="stylesheet" href="/js/fancybox/helpers/jquery.fancybox-buttons.css?v=1.0.5" type="text/css" media="screen" />
-		<script src="/js/fancybox/helpers/jquery.fancybox-buttons.js?v=1.0.5"></script>
-		<script src="/js/fancybox/helpers/jquery.fancybox-media.js?v=1.0.5"></script>
-
-		<link rel="stylesheet" href="/js/fancybox/helpers/jquery.fancybox-thumbs.css?v=1.0.7" type="text/css" media="screen" />
-		<script src="/js/fancybox/helpers/jquery.fancybox-thumbs.js?v=1.0.7"></script>
+		<script src="/js/jquery-1.9.0.min.js"></script>
 		
         <!--[if lt IE 9]>
         <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-        <![endif]--> 
+        <![endif]-->
+
     </head>
 
     <body>
 	
-		<div id="wrapper">
+<div id="wrapper">
 
-		<div class="hover flipcard">
+		<section class="card section">
+			<img src="img/rolle-128x128.png" alt="Rolle" />
+	
+			<header class="information">
+					
+				<h1>Roni Laukkarinen</h1> 
+				<p>Front End Web developer <a href="http://www.dude.fi">@dude</a>. Loves SASS, PHP, jQuery and Macbook Pro.</p>
 
-				<div class="front">
-					<img src="images/me-300x300.png" alt="Picture of me! Roni Laukkarinen, nice to meet you!" />
-				</div>
+				<p>But that's not all. I'm also...</p>
 
-				<div class="back">
-					<p><strong><a href="https://plus.google.com/116255313263451216782?rel=author">Roni Laukkarinen</a></strong><br />
-						Jyväskylä, Finland<br />
-						<a href="?vcard">vcard</a> / <a href="?json">json</a></p>
-				</div>
+<!-- 				<ul>
+				<li>Film enthusiast</li>
+				<li>General geek</li>
+				<li>A father of one son and one daughter</li>
+				<li>A husband to a deafblind person</li>
+				<li>First entrepreneur in my family tree</li>
+				<li>Owner of multiple blogs</li>
+				</ul> -->
 
-		</div>
-		
-		<header id="header">
-			<h1><?php $greets = array(
-			
-			'Ya at eeh! <span>(Tervehdys navajoksi)</span>',
-			'Aloha! <span>(Tervehdys hawaiiksi)</span>',
-			"Ba'ax ka wa'alik! <span>(Tervehdys mayaksi)</span>",
-			"Ia! Ia shugg! <span>(Tervehdys H.P. Lovecraftin R'lyehian-kielellä)",
-			"Suilad! <span>(Tervehdys Sindarin-haltiakielellä Taru Sormusten Herrasta)</span>",
-			"Aiya! <span>(Tervehdys Quenya-haltiakielellä Taru Sormusten Herrasta)</span>",
-			"NuqneH! <span>(Tervehdys klingoniksi Star Trekistä)</span>",
-			"Onak sha sree! <span>(Tervehdys goa'uldiksi Tähtiportista)</span>",
-			"Kel shak! <span>(Tervehdys goa'uldiksi Tähtiportista)</span>",
-			"M'athchomaroon! <span>(Tervehdys dothrakiksi Game of Thronesista)</span>"
-			
-			); 
-			$randomgreet = array_rand($greets); echo $greets[$randomgreet]; ?></h1>
-        </header>
+			</header>
 
-		<div id="whois" class="area">
+		</section>
 
-			<p><strong>Moro! Kiva kun kiinnostuit.</strong> Nimeni on Roni, mutta netissä ja muuallakin minua sanotaan Rolleksi. Tämä sivu on yksinkertainen käyntikorttini ja infosivuni. Tervetuloa!</p>
+	<div class="infos">
 
-			<p class="email"><span class="icon"></span><span class="email">roni@laukkarinen.info</span></p>
+		<section class="finland section">
 
-			<p class="onlineblock">Olen juuri nyt <?php
-$awayinfo=file('http://peikko.us/status.log');
-foreach ($awayinfo as $awayinfot);
-$awayinfot = utf8_encode($awayinfot); // ääkköset näkyviin
-$tila_raw = explode('|', $awayinfot);
-$status = $tila_raw[0];
-$syy = $tila_raw[1];
+			<span class="fa fa-flag"></span>
+			<h2>From Finland</h2>
 
-if ($status == "paikalla") { echo '<span class="online status"><a href="javascript:void(0)" class="blob"><span class="info">Bitlbee-palvelimeni yhdistää yleisimmät pikaviestimet samaan. Olen samaan aikaan paikalla kaikkialla.</span></a><span class="status-text">paikalla</span>'; } 
-else { echo '<span class="away status"><a href="javascript:void(0)" class="blob"><span class="info">Bitlbee-palvelimeni yhdistää yleisimmät pikaviestimet samaan. Olen samaan aikaan paikalla kaikkialla.</span></a><span class="status-text">poissa</span>'; }
-echo '</span>';
-//echo "<br /><a onclick=\"window.open('http://www.plugoo.com/chat/run.php?go=G2P3BZSDWYHDG9G&amp;sk=3&amp;height=300&amp;width=140','',CONFIG='HEIGHT=300,WIDTH=140,TOOLBAR=no,MENUBAR=no,SCROLLBARS=no,RESIZABLE=no,LOCATION=no,DIRECTORIES=no,STATUS=no')\" href=\"javascript:void(0)\">";
+			<p>The land of Angry Birds, sauna, Nokia mobile phones, etc.</p>
 
-//echo " &mdash; <a href=\"javascript:;\" onclick=\"plupper.start('rolle@plupper.com')\">";
-// if ($status == "paikalla") { echo 'Avaa reaaliaikainen chat'; } 
-// else { echo 'Jätä viesti ja yhteystietosi'; }
-//echo '</a>';
+		</section>
 
-?></p>
+		<section class="beer section">
 
-		</div>
+			<span class="fa fa-beer"></span>
+			<h2>Beer enthusiast</h2>
 
-		<div id="profession" class="details">
+			<p>I love craft beer. Keeping track with <a href="http://untappd.com/user/rolle">Untappd</a> and writing a finnish beer blog at <a href="http://www.huurteinen.fi">huurteinen.fi</a>.</p>
 
-			<h2>Ammattitaito</h2>
-			<p>Olen ammatiltani web-alan yrittäjä, <strong>web-suunnittelija</strong>/web-kehittäjä omassa firmassani <a href="http://www.dude.fi">Digitoimisto Dude Oy:ssa</a>. Toisin sanoen teen työkseni nettisivuja, kotisivuja, verkkototeutuksia, mainoksia, ulkoasuja, koodailuja, verkkokauppoja, mitä ikinä netti pitääkään sisällään.</p>
+		</section>
 
-			<p>Ansioluetteloni ja portfolioni löytyvät sosiaalisesta mediasta (kolme kuvaketta alla). <strong>Jos tarvitset sivuja tai muuta nettiin liittyvää</strong>, pistä viestiä minkä kanavan kautta tahansa, esim. <a href="http://www.dude.fi/yhteystiedot/#poks">oman yritykseni (Digitoimisto Dude Oy) yhteystietolomakkeesta</a> tai <a href="http://twitter.com/rolle">Twitterissä</a>.</p>
+		<section class="beer section">
 
-			<ul>
-				<li class="resume"><a href="http://www.linkedin.com/in/rolaukka" target="_blank" id="linkedin" title="LinkedIN-ansioluetteloni">LinkedIN</a></li>
-				<li class="portfolio"><a href="http://be.net/rolle" target="_blank" id="behance" title="Behance-portfolioni">Behance</a></li>
-				<li class="github last"><a href="http://github.com/ronilaukkarinen" target="_blank" id="github" title="Github-koodikirjastoni">Github</a></li>
-			</ul>
+			<span class="fa fa-music"></span>
+			<h2>Metalhead</h2>
 
-		</div>
+			<p>Been love in heavy metal since forever. Check out my <a href="http://www.last.fm/user/rolle-">last.fm</a> to find out more. Journalist at <a href="http://www.metallimusiikki.net">metallimusiikki.net</a> webzine since 2007.</p>
 
-		<div id="websites" class="details">
+		</section>
 
-			<h2>Nettisivut</h2>
+		<section class="films section">
 
-			<p>Aloitin puuhailun nettisivujen parissa 90-luvulla omaksi ilokseni. Siitä lähtien olen ollut koukussa ja Internet on ollut minulle arkipäivää. Olen toteuttanut lukuisia verkkoratkaisuja, en pelkästään itselleni vaan myös yrityksille, organisaatioille ja yksityisille tahoille. Joitakin vapaa-ajan projektejani löydät alta.</p>
+			<span class="fa fa-film"></span>
+			<h2>Movie enthusiast</h2>
 
-			<ul>
-				<li class="problemsolvin"><a href="http://www.problemsolv.in" target="_blank" title="My IT-blog in english">Problemsolv.in</a></li>
-				<li class="peikko second"><a href="http://www.peikko.us" target="_blank" title="Kotipalvelimeni">Peikko.us</a></li>
-				<li class="rollemaa last second"><a href="http://www.rollemaa.org" target="_blank" title="Ensimmäisiä verkkosivustojani">Rollemaa.org</a></li>
-			</ul>
-		</div>
+			<p>Movies are my way to relax and escape reality (we all need that sometimes). Writing <a href="http://www.rollemaa.org/leffat">a finnish movie blog</a> and short reviews in english <a href="http://letterboxd.com/rolle/">at Letterboxd</a>.</p>
 
-		<div id="socialmedia" class="details">
+		</section>
 
-			<h2>Sosiaalinen media</h2>
+	</div><!--/.infos-->
+	
 
-			<p>Suomalainen keksintö, samalla Internet-historian vanhin pikaviestin IRC keksittiin syntymävuotenani 1988. Olin käyttänyt nettiä ensisijaisena kommunikaatiovälineenä jo kauan ennen kuin tekstipohjaiset IM-sovellukset, purkit, irkit ja foorumit pikkuhiljaa muuttuivat tämän päivän 'sosiaaliseksi mediaksi'.</p>
+</div><!--/!the end of good ol' #wrapper -->
 
-			<p>Testailen innolla uusia sosiaalisen median ratkaisuja ja nykyään olen mukana seuraamassa yli kuudenkymmenen sosiaalisen median palvelun kehitystä. Alla itselleni tällä hetkellä tärkeimpiä.</p>
-
-			<ul>
-				<li class="facebook"><a title="Stalk me on Facebook!" target="_blank" href="http://facebook.com/rollefb">Facebook</a></li>
-				<li class="googleplus second"><a title="Add me to your circles at Google+!" target="_blank" href="http://gplus.to/rolle">Google+</a></li>
-				<li class="twitter"><a title="Follow me in Twitter!" target="_blank" href="http://twitter.com/rolle">Twitter</a></li>
-				<li class="youtube last second"><a title="Subscribe to my videos in YouTube!" target="_blank" href="http://youtube.com/user/rollex2">Youtube</a></li>
-				<li class="lastfm"><a title="See my taste in music in Last.fm!" target="_blank" href="http://www.last.fm/user/rolle-/">Last.fm</a></li>
-				<li class="foursquare second"><a title="See where I go in Foursquare!" target="_blank" href="http://foursquare.com/rolle">Foursquare</a></li>
-				<li class="instagram"><a title="View my pics in Instagram!" target="_blank" href="http://instagram.com/rolle_">Instagram</a></li>
-				<li class="delicious last second"><a title="View my bookmarks in Delicious!" target="_blank" href="http://delicious.com/rollex">Delicious</a></li>
-				<li class="getglue"><a title="Check out what I watch and listen at GetGlue!" target="_blank" href="http://getglue.com/rolle_">GetGlue</a></li>
-				<li class="untappd second"><a title="View my beer reviews in Untappd!" target="_blank" href="http://untappd.com/user/rolle">Untappd</a></li>
-				<li class="letterboxd"><a title="View my movie reviews in Letterboxd!" target="_blank" href="http://letterboxd.com/rolle">Letterboxd</a></li>
-			</ul>
-
-		</div>
-
-		<footer id="footer">
-
-			<p>Säädän jatkuvasti jonkun prokkiksen parissa, oli sitten kyseessä sovellus, sivusto tai scripti. Tämäkin sivu näytti vuonna 2012 vähän erilaiselta. <a href="2012">Tsekkaa, jos kiinnostaa.</a></p>
-
-			<p>Lisää mahtavaa settiä tulossa pian. Minua ei harmita kiireisyys &mdash; se pitää minut järjissäni.</p>
-			<p class="reclamation">Tämä sivu on rakennettu täysin tyhjästä 25. tammikuuta 2013. Vimeisin pikkupäivitys 30. joulukuuta 2013 (sitä ennen 25. kesäkuuta 2013).</p>
-
-		</footer>
-
-		</div>
-		
-<div class="forenglish">
-
-<strong>Strange language?</strong> It's finnish! But hey, I understand if you don't get a word. Check out this page in english <a href="index_en.php">here!</a>
-
-</div>
 
 <script>
   var _gaq = _gaq || [];
@@ -195,35 +109,6 @@ echo '</span>';
 </script>
 
 <script src="https://www.google.com/jsapi"></script>
-
-		<script>		
-		// $(function(){
-
-		// 	$("#websites-title a").click(function(){
-  		// 			$("#websites-title").hide();
-  		// 			$("#websites-block").fadeIn(1000);
-		// 	});
-
-			$(".fancy").fancybox({
-				maxWidth	: 800,
-				maxHeight	: 600,
-				fitToView	: false,
-				width		: '70%',
-				height		: '70%',
-				autoSize	: false,
-				closeClick	: false,
-				openEffect	: 'none',
-				closeEffect	: 'none'
-			});
-
-			$('.hover').hover(function(){
-				$(this).addClass('flip');
-			},function(){
-				$(this).removeClass('flip');
-			});
-
-		// });
-		</script>
 
     </body>
 </html>
